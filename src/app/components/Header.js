48346dd7233bd2d7db2fe237883c4b9346798aa1
jin lee <jjin_0627@hotmail.com>
2024-07-14 12:29:44 -0700

    text extraction + header styling

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-container">
+        <a href="/">
+        <p>ruleaid</p>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
