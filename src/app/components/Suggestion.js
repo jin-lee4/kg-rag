73a1a084ef9f281ef2ee71a2b6bc8a00e947b9de
jin-lee4 <61896039+jin-lee4@users.noreply.github.com>
2024-07-29 12:13:32 -0700

    upload/loading states, suggestion options

--- src/app/components/Suggestion.js
+++ src/app/components/Suggestion.js
@@ -1,24 +1,30 @@
-"use client"; // Add this line
+"use client";
 
 import React, { useState } from "react";
+import SuggestionOptions from "./SuggestionOptions";
 
 const Suggestion = ({ borderColor, title, description }) => {
-  // const [chatMessage, setChatMessage] = useState('');
-  // const [chatHistory, setChatHistory] = useState([]);
+  const [chatMessage, setChatMessage] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const [isChatVisible, setIsChatVisible] = useState(false);
 
-  // const handleChatChange = (e) => {
-  //   setChatMessage(e.target.value);
-  // };
+  const handleChatChange = (e) => {
+    setChatMessage(e.target.value);
+  };
 
-  // const handleSubmit = () => {
-  //   // Add your submit logic here
-  //   setChatHistory([...chatHistory, { text: chatMessage, sender: 'user' }]);
-  //   setChatMessage('');
-  // };
+  const handleSubmit = () => {
+    // Add your submit logic here
+    setChatHistory([...chatHistory, { text: chatMessage, sender: "user" }]);
+    setChatMessage("");
+  };
 
-  // const handleCancel = () => {
-  //   setChatMessage('');
-  // };
+  const handleCancel = () => {
+    setChatMessage("");
+  };
+
+  const toggleChatVisibility = () => {
+    setIsChatVisible(!isChatVisible);
+  };
 
   return (
     <div
@@ -26,12 +32,46 @@ const Suggestion = ({ borderColor, title, description }) => {
       style={{ border: `3px solid ${borderColor}` }}
     >
       <div>
-        <p id="suggestion-mode" className="small-text">{title}</p>
+        <p id="suggestion-mode" className="small-text">
+          {title}
+        </p>
       </div>
       <div id="suggestion-text">
         <p>{description}</p>
       </div>
-      <div></div>
+      {isChatVisible && (
+        <div id="chat-container">
+          <div id="input-container">
+            {chatHistory.map((message, index) => (
+              <div
+                key={index}
+                style={{
+                  marginBottom: "4px",
+                  textAlign: message.sender === "user" ? "right" : "left",
+                }}
+              >
+                {message.text}
+              </div>
+            ))}
+          </div>
+          <input
+            type="text"
+            value={chatMessage}
+            onChange={handleChatChange}
+            placeholder="Ask ruleaid..."
+            id="message-input"
+          />
+          <div id="message-buttons-container">
+            <button onClick={handleCancel} id="cancel-message">
+              Cancel
+            </button>
+            <button onClick={handleSubmit} id="submit-message">
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
+      <SuggestionOptions onStartChat={toggleChatVisibility} />
     </div>
   );
 };
--- src/app/components/Suggestions.js
+++ src/app/components/Suggestions.js
@@ -1,7 +1,7 @@
 "use client"; // Add this line
 
-import React from 'react';
-import Suggestion from "./Suggestion"; 
+import React from "react";
+import Suggestion from "./Suggestion";
 
 const Suggestions = () => {
   return (
@@ -24,8 +24,71 @@ const Suggestions = () => {
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua."
       />
+      <Suggestion
+        borderColor="#9d87d9"
+        title="Analyse"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Suggestion
+        borderColor="#9d87d9"
+        title="Analyse"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Suggestion
+        borderColor="#9d87d9"
+        title="Analyse"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Suggestion
+        borderColor="#9d87d9"
+        title="Analyse"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Suggestion
+        borderColor="#9d87d9"
+        title="Analyse"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Suggestion
+        borderColor="#9d87d9"
+        title="Analyse"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Suggestion
+        borderColor="#9d87d9"
+        title="Analyse"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Suggestion
+        borderColor="#9d87d9"
+        title="Analyse"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
     </div>
   );
 };
+
+// {suggestions.map((suggestion, index) => (
+//   <Suggestion
+//     key={index}
+//     borderColor={
+//       suggestion.mode === "Analyze"
+//         ? "#9d87d9"
+//         : suggestion.mode === "Compare"
+//         ? "#e08072"
+//         : "#7cc287"
+//     }
+//     title={suggestion.mode}
+//     description={suggestion.suggestions}
+//   />
+// ))}
 
 export default Suggestions;
